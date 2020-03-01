231957472112df6fbce3d414bea2a9e4578835bb
Jonathan Jeffrey <jjeffrey6@gatech.edu>
2020-02-29 19:20:09 -0500

    Make inventory search autocomplete instead

--- src/api/inventory.ts
+++ src/api/inventory.ts
@@ -6,6 +6,12 @@ export default {
     const data = await sharedAxios.put('/inventory/class/upsert', item);
     return data.data;
   },
+  async listInventoryCheckoutItems(): Promise<String[]> {
+    const data = await sharedAxios.get('/v1/inventory/checkout/items');
+    return data.data.data.map((obj: any) => {
+      return obj.ItemName;
+    });
+  },
   async listInventoryItemClass(page = 0, pageSize: number = 100): Promise<ResponseObject<IHiveInventoryItemClass[]>> {
     const data = await sharedAxios.get('/inventory/class/list', {
       params: {
--- src/pages/inventory/InventorySearch.tsx
+++ src/pages/inventory/InventorySearch.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { Props } from 'react';
+
+import api from '../../api/inventory';
 
 import styles from './InventorySearch.module.scss';
-import {Button, Col, Row, Input} from 'antd';
+import {Button, Col, Row, Input, AutoComplete} from 'antd';
 
-export default class InventorySearch extends React.Component {
+export default class InventorySearch extends React.Component<any, any> {
+  constructor(param: Props<any>) {
+    super(param);
+
+    this.state = {
+      value: '',
+      dataSource: []
+    };
+  }
+
+  componentDidMount() {
+    (async() => {
+      try {
+        const a = await api.listInventoryCheckoutItems();
+        console.log(a);
+        this.setState({
+          dataSource: a
+        });
+      } catch (e) {
+      }
+    })();
+  }
+
   render() {
     return (
       <div className="hive-container">
         <div className={styles.centerOuterContainer}>
           <Row type="flex" justify="center">
             <Col span={20} className={styles.inventoryTitle}>
-              <h2>Search Hive Inventory</h2>
+              <h2>Checkout Form</h2>
             </Col>
           </Row>
           <Row type="flex" justify="center" className={styles.inventorySearchBar}>
             <Col span={20}>
-              <Input.Search
+              <AutoComplete
+                dataSource={this.state.dataSource}
+                filterOption={(input: any, option: any) => {
+                  return option.key.toLowerCase().indexOf(input.toLowerCase()) !== -1;
+                }}
+                // onSearch={}
                 placeholder="Type something to begin. For example: Arduino"
-                enterButton="Search"
-                size="large"
-                onSearch={value => console.log(value)}
               />
             </Col>
           </Row>
@@ -27,4 +53,7 @@ export default class InventorySearch extends React.Component {
       </div>
     );
   }
+}
+
+class Complete extends React.Component {
 }
